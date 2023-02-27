5d51d1b7a993cea271a38c325fd510fa91761185
Rajashan <raja.zaker@gmail.com>
2023-02-27 23:19:11 +0100

    change phenotype building

--- phenotype.py
+++ phenotype.py
@@ -1,6 +1,7 @@
+import jax
 import jax.numpy as jnp
 from jax import random
-from jax.experimental.stax import Dense, Relu, serial
+from jax.example_libraries.stax import Dense, Relu, serial
 
 # Define a function to translate the genome into phenotype
 def translate_genome(genome):
@@ -16,43 +17,63 @@ def evaluate_fitness(genome):
     return fitness
 
 
-def translate_genome(genome):
-    # Extract relevant information from genome
-    input_size = genome.input_size
-    output_size = genome.output_size
-    node_genes = genome.node_genes
-    conn_genes = genome.conn_genes
+def genome_to_network(genome):
+    # Extract the node and connection genes from the genome
+    node_genes = genome.nodes
+    connection_genes = genome.connections
+    
+    # Determine the number of input, output, and hidden nodes
+    num_inputs = sum(1 for node_gene in node_genes if node_gene.node_type == 'input')
+    num_outputs = sum(1 for node_gene in node_genes if node_gene.node_type == 'output')
+    num_hidden = len(node_genes) - num_inputs - num_outputs
+    
+    # Define the activation function for the nodes
+    def activation(x):
+        return jnp.tanh(x)
+    
+    # Define the forward pass of the network
+    def forward(params, x):
+        # Extract the weights from the parameters
+        input_weights = params['input_weights']
+        hidden_weights = params['hidden_weights']
+        output_weights = params['output_weights']
+        
+        # Calculate the output of the input layer
+        input_layer_output = activation(jnp.dot(x, input_weights))
+        
+        # Calculate the output of the hidden layers
+        hidden_layer_outputs = []
+        for i in range(num_hidden):
+            # Calculate the input to the i-th hidden node
+            node_input = 0.0
+            for connection_gene in connection_genes:
+                if connection_gene.out_node == node_genes[num_inputs + i].node_id and connection_gene.enabled:
+                    in_node_index = next((j for j, node_gene in enumerate(node_genes) if node_gene.node_id == connection_gene.in_node), None)
+                    node_input += connection_gene.weight * input_layer_output[in_node_index]
+            # Calculate the output of the i-th hidden node
+            hidden_layer_outputs.append(activation(node_input))
+        hidden_layer_output = jnp.array(hidden_layer_outputs)
+        
+        # Calculate the output of the output layer
+        output_layer_output = jnp.dot(hidden_layer_output, output_weights)
+        
+        return output_layer_output
+    
+    # Initialize the weights
+    input_weights_shape = (num_inputs, num_hidden)
+    hidden_weights_shape = (num_hidden, num_hidden)
+    output_weights_shape = (num_hidden, num_outputs)
+    input_weights = jnp.zeros(input_weights_shape)
+    hidden_weights = jnp.zeros(hidden_weights_shape)
+    output_weights = jnp.zeros(output_weights_shape)
+    params = {'input_weights': input_weights, 'hidden_weights': hidden_weights, 'output_weights': output_weights}
+    
+    # Set the weights based on the connection genes
+    for connection_gene in connection_genes:
+        if connection_gene.enabled:
+            if node_genes[connection_gene.out_node].node_type == 'hidden':
+                out_node_index = next((i for i, node_gene in enumerate(node_genes) if node_gene.node_id == connection_gene.out_node), None) - num_inputs
+                in_node_index = next((i for i, node_gene in enumerate(node_genes) if node_gene.node_id == connection_gene.in_node), None)
+                params['hidden_weights'][in_node_index, out_node_index] = connection_gene.weight
 
-    # Create a list of layer specifications for the neural network
-    layers = []
-    for node_id, node_gene in node_genes.items():
-        if node_gene.enabled:
-            if node_id < input_size:
-                layers.append(Dense(node_gene.num_outputs, input_shape=(input_size,)))
-            else:
-                layers.append(Dense(node_gene.num_outputs))
-
-    # Sort the connection genes by their innovation number
-    conn_genes = sorted(conn_genes.values(), key=lambda x: x.innovation_number)
-
-    # Create the neural network by adding connections to the layers
-    for conn_gene in conn_genes:
-        if conn_gene.enabled:
-            input_id = conn_gene.input_node_id
-            output_id = conn_gene.output_node_id
-            weight = conn_gene.weight
-            if input_id < input_size:
-                layer = layers[output_id - input_size]
-                layer = serial(layer, Relu)
-                layers[output_id - input_size] = layer
-            else:
-                layer = layers[output_id - input_size]
-                layer = serial(Dense(layer[0], W_init=jnp.zeros, b_init=jnp.zeros), layer[1])
-                layers[output_id - input_size] = layer
-
-            layer = layers[output_id - input_size]
-            layer = serial(Dense(layer[0], W_init=jnp.zeros, b_init=jnp.array([weight])), layer[1])
-            layers[output_id - input_size] = layer
-
-    # Create the final neural network by combining all the layers
-    return serial(*layers, Dense(output_size))
+    return jax.jit(forward), params
