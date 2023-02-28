d3a48b660b5d734dab8a5b1de51ed2ef903e5d75
Rajashan <raja.zaker@gmail.com>
2023-02-28 15:05:37 +0100

    move forward pass out of parameter building

--- phenotype.py
+++ phenotype.py
@@ -17,7 +17,7 @@ def evaluate_fitness(genome):
     return fitness
 
 
-def genome_to_network(genome):
+def genome_to_network_params(genome):
     # Extract the node and connection genes from the genome
     node_genes = genome.nodes
     connection_genes = genome.connections
@@ -27,38 +27,6 @@ def genome_to_network(genome):
     num_outputs = sum(1 for node_gene in node_genes if node_gene.node_type == 'output')
     num_hidden = len(node_genes) - num_inputs - num_outputs
     
-    # Define the activation function for the nodes
-    def activation(x):
-        return jnp.tanh(x)
-    
-    # Define the forward pass of the network
-    def forward(params, x):
-        # Extract the weights from the parameters
-        input_weights = params['input_weights']
-        hidden_weights = params['hidden_weights']
-        output_weights = params['output_weights']
-        
-        # Calculate the output of the input layer
-        input_layer_output = activation(jnp.dot(x, input_weights))
-        
-        # Calculate the output of the hidden layers
-        hidden_layer_outputs = []
-        for i in range(num_hidden):
-            # Calculate the input to the i-th hidden node
-            node_input = 0.0
-            for connection_gene in connection_genes:
-                if connection_gene.out_node == node_genes[num_inputs + i].node_id and connection_gene.enabled:
-                    in_node_index = next((j for j, node_gene in enumerate(node_genes) if node_gene.node_id == connection_gene.in_node), None)
-                    node_input += connection_gene.weight * input_layer_output[in_node_index]
-            # Calculate the output of the i-th hidden node
-            hidden_layer_outputs.append(activation(node_input))
-        hidden_layer_output = jnp.array(hidden_layer_outputs)
-        
-        # Calculate the output of the output layer
-        output_layer_output = jnp.dot(hidden_layer_output, output_weights)
-        
-        return output_layer_output
-    
     # Initialize the weights
     input_weights_shape = (num_inputs, num_hidden)
     hidden_weights_shape = (num_hidden, num_hidden)
@@ -67,7 +35,7 @@ def genome_to_network(genome):
     hidden_weights = jnp.zeros(hidden_weights_shape)
     output_weights = jnp.zeros(output_weights_shape)
     params = {'input_weights': input_weights, 'hidden_weights': hidden_weights, 'output_weights': output_weights}
-    
+    """
     # Set the weights based on the connection genes
     for connection_gene in connection_genes:
         if connection_gene.enabled:
@@ -75,5 +43,11 @@ def genome_to_network(genome):
                 out_node_index = next((i for i, node_gene in enumerate(node_genes) if node_gene.node_id == connection_gene.out_node), None) - num_inputs
                 in_node_index = next((i for i, node_gene in enumerate(node_genes) if node_gene.node_id == connection_gene.in_node), None)
                 params['hidden_weights'][in_node_index, out_node_index] = connection_gene.weight
+    """
+    return params
 
-    return jax.jit(forward), params
+def activation(x):
+        return jnp.tanh(x)
+
+def forward(params, x):
+     pass
